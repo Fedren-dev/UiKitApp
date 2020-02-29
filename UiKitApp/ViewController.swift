1fba8989ee955a3ee4cfe2b9396f9e8344fe9982
Alexander <fedrennn@gmail.com>
2020-02-29 07:53:59 +0200

    Initial Commit

---
+++ UiKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UiKitApp
+//
+//  Created by Александр Федоров on 29.02.2020.
+//  Copyright © 2020 Alexander Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
